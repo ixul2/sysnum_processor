d6a5720f374db0578676e03b7b7ad279eff9da72
Lucas Hermier--Gastineau <lhermiergastinea@clipper.ens.fr>
2026-01-26 17:16:48 +0100

    checked r3

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-compilateur/assembler.exe $1 -o rom  && ./netlist_simulator/netlist_simulator.exe processor_netlists rom
+compilateur/assembler.exe $1 -o rom  && ./netlist_simulator/netlist_simulator.exe -d -n 50 processor_netlists rom
