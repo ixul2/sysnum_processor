f7f7c7af161decde5250dcede8ab2c7fc50bba13
Lucas Hermier--Gastineau <lhermiergastinea@clipper.ens.fr>
2026-01-26 15:42:14 +0100

    added readme

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-compilateur/assembler.exe $1 -o _build/rom  && ./netlist_simulator/netlist_simulator.exe processor_netlists _build/rom
+compilateur/assembler.exe $1 -o rom  && ./netlist_simulator/netlist_simulator.exe processor_netlists rom
