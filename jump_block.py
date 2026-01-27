16dfc90373e6a0e8c85f37a6ccebfb5937479d7a
Alixe <hugo.dethorey@ens.psl.eu>
2026-01-27 11:29:40 +0100

    rm .py

--- jump_block.py
+++
@@ -1,9 +0,0 @@
-from lib_carotte import *
-from settings import *
-
-allow_ribbon_logic_operations(True)
-
-
-def jump_block(V,N,Z,Sup,Eq,Inf,Jmp):
-	is_jump = Jmp & (((Sup & (~N)) & (~Z)) | (N & Inf) | (Z & Eq))
-	return is_jump
